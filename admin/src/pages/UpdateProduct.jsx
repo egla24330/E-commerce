b8534f4352bd664595acd8fd49fa3e0de9856d70
egla24330 <egla24330@gmail.com>
2025-06-01 16:57:55 -0300

    Temporary commit before pulling

--- admin/src/pages/UpdateProduct.jsx
+++ admin/src/pages/UpdateProduct.jsx
@@ -213,7 +213,7 @@ const UpdateProduct = () => {
                 value={product.price}
                 onChange={handleChange}
                 min="0"
-                step="0.01"
+               
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
                 required
               />
