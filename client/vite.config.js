04b041ff6393a4f96fbb8a6f437723694c9cb7ac
egla24330 <egla24330@gmail.com>
2025-06-03 15:13:51 -0300

    Fix client build settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   build: {
-    outDir: path.resolve(__dirname, '../backend/dist'), // ✅ THIS sends the build into backend/dist
-    emptyOutDir: true // optional: deletes old files
-  }
-})
+    outDir: path.resolve(__dirname, '../backend/dist'), // send build output to backend
+    emptyOutDir: true,
+  },
+});
