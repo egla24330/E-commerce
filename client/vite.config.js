5cb5f176778df16fa9c77662260953c6dad9360f
egla24330 <egla24330@gmail.com>
2025-06-03 16:31:36 -0300

    Add _redirects file to support client-side routing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'static/_redirects', // relative to `client/`
-          dest: '.' // put in root of `dist/`
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, '../backend/dist'),
+    outDir: 'build', // ✅ Match what Render expects
     emptyOutDir: true,
   },
 });
