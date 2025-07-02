f84b45516ba44b57a6503e7c27fcd711ca6513d6
egla24330 <egla24330@gmail.com>
2025-07-02 15:30:07 -0300

    fix dir

--- backend/bot/index.js
+++ backend/bot/index.js
@@ -1,5 +1,6 @@
 import TelegramBot from 'node-telegram-bot-api';
 import path from 'path';
+import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 function startBot() {
